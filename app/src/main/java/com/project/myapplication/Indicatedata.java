62b26fab754a4740933200181d753ac7ee156228
nima <nimaabdpoor@gmail.com>
2020-01-05 14:11:07 +0330

    selecting Firstname from people table successfully done.indicate in Log(checkdata).

---
+++ app/src/main/java/com/project/myapplication/Indicatedata.java
@@ -0,0 +1,44 @@
+package com.project.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+public class Indicatedata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indicatedata);
+        showtable();
+//        ListView list_people=findViewById(R.id.list_people);
+//        ArrayList<peopleinfo> peoples=new ArrayList<peopleinfo>();
+//        LinkedHashMap<String,String> samplepeopledata=new LinkedHashMap<String, String>();
+//        Set<String> keyset=samplepeopledata.keySet();
+//        for (String key:keyset) {
+//            String value =samplepeopledata.get(key);
+//            peoples.add(new peopleinfo(key,value,null,null,null,null,null));
+//        }
+//        peopleListViewAdapter adapter=new peopleListViewAdapter(this,peoples);
+//        list_people.setAdapter(adapter);
+    }
+
+    private void showtable() {
+        FeedreaderDBHelper Fhelper=new FeedreaderDBHelper(this);
+        SQLiteDatabase db=Fhelper.getWritableDatabase();
+        Cursor cursor=db.rawQuery("select * from people",null);
+        while (cursor.moveToNext()) {
+            String fristname=cursor.getString(cursor.getColumnIndex("firstname"));
+            Log.i("checkdata",fristname);
+        }
+        cursor.close();
+    }
+}
